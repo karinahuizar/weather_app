7847aecdbc280e71e3cbba5eeab8a3198d02ff48
Karina Huizar <karinahuizar@gmail.com>
2021-06-08 08:41:32 -0500

    Removed extra <div>

--- weather.js
+++ weather.js
@@ -51,7 +51,7 @@ function displayForecast() {
           <div class="weather-forecast-temperature">
           <span class="weather-forecast-temperature-max">25°</span>|
           <span class="weather-forecast-temperature-min">10°</span>
-          </div>
+          
         </div>
       </div>`;
   });
