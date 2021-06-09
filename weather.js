370c0783ab5bfdac0b7830b6548664b03fc3af4c
Karina Huizar <karinahuizar@gmail.com>
2021-06-08 21:31:05 -0500

    Added forecast from API.

--- weather.js
+++ weather.js
@@ -33,28 +33,46 @@ let dia = document.querySelector("#currentDate");
 let hora = document.querySelector("#currentTime");
 dia.innerHTML = `${day}, ${hour}:${minute}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Mon", "Tue", "Wed", "Thu"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
           <br />
-          <i class="fas fa-sun"></i>
-          <br />
-          <br />
+          <img
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
+            alt=""
+            width="42"
+             />
           <div class="weather-forecast-temperature">
-          <span class="weather-forecast-temperature-max">25°</span>|
-          <span class="weather-forecast-temperature-min">10°</span>
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}°</span>|
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}°</span>
         </div>
       </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
